e8690de0ba0204d2e0d33530663a74e96bb04ae3
kup-kup <konka.um@gmail.com>
2026-02-22 10:12:37 +0700

    refactor as scope changes

--- wordle.py
+++ wordle.py
@@ -1,160 +1,98 @@
 import random
 from collections import defaultdict
+from dataclasses import dataclass
+
+with open('valid_words.txt') as f:
+    VALID_WORDS = f.read().splitlines()
+
+with open('valid_answers.txt') as f:
+    VALID_ANSWERS = f.read().splitlines()
+
+# @dataclass()
+class Constraints:
+    correct_position: list[None | str] = [None]*5
+    wrong_position: list[set] = [set()]*5
+    missing: set = set()
+    min_count: dict[str, int] = defaultdict(int)
+    max_count: dict[str, int] = defaultdict(int)
+
+def get_incremental_constraints(consts: Constraints, step_consts: Constraints) -> Constraints:
+    incremental_consts = Constraints()
+    word_length = len(consts.correct_position)
+    assert word_length == len(step_consts.wrong_position)
+
+    # handle green
+    for i in range(word_length):
+        if not consts.correct_position[i] and step_consts.correct_position[i]:
+            incremental_consts.correct_position[i] = step_consts.correct_position[i]
+    
+    # handle yellow
+    for i in range(word_length):
+        if not step_consts.wrong_position[i]:
+            continue
+        for step_yellow in step_consts.wrong_position[i]:
+            if step_yellow not in consts.wrong_position[i]:
+                incremental_consts.wrong_position[i].add(step_yellow)
+
+    # handle gray
+    incremental_consts.missing = step_consts.missing - consts.missing
+
+    # handle count
+    for k, v in step_consts.min_count.items():
+        if consts.min_count[k] < v:
+            incremental_consts.min_count[k] = v
+    
+    for k, v in step_consts.max_count.items():
+        if consts.max_count.get(k, float('inf')) > v:
+            incremental_consts.max_count[k] = v
+
+    return incremental_consts
 
 class Wordle:
 
     def __init__(
             self, 
-            init_guesses = None, 
             answer = None,
             turns = 6, 
         ):
         
         self.turns = turns
         self.feedback = []
-        self.guesses = init_guesses if init_guesses else []
+        self.guesses = []
         self.word_length = 5 # no support for other lengths for now
         assert answer is None or len(answer) == 5
 
-        # self.word_length = len(answer) if answer else 5 
-        # word_length needs to be 5 if no answer is provided
-        # as the class randomizes answers from 5 letters word list
-        
-        self.constraints = self._get_constraint_holder()
-
-        # initialize valid words
-        with open('valid_words.txt') as f:
-            self.valid_words = f.read().splitlines()
-        
-        # initialize valid answers and possible answers
-        with open('valid_answers.txt') as f:
-            self.valid_answers = f.read().splitlines()
-        self.possible_answers = set(self.valid_answers)
-
         # initialize answer
         if answer is None:
-            self.answer = random.choice(self.valid_answers)
+            self.answer = random.choice(VALID_ANSWERS)
         else:
             self.answer = answer
-        
-        # initialize letter_index
-        self.letter_index = defaultdict(list)
-        for i in range(self.word_length):
-            self.letter_index[self.answer[i]].append(i)
-
-        for guess in self.guesses:
-            self.add_guess(guess)
-
-    def _get_constraint_holder(self):
-        return {
-            'green': [None] * self.word_length,
-            'yellow': [set() for _ in range(self.word_length)],
-            'gray': set(), # represents letters not present in answer as it is easier to handle
-            # In normal wordle, gray can includes letters that exist in a word
-            # but that slot is taken by another letter in the guessing word. 
-            # For example, for the answer "later" and guess "troll". The first 
-            # "l" in "troll" would be yellow but the second one would be gray,
-            # even though the answer has "l" in it.
-
-            'min_count': defaultdict(int), 
-            'max_count': defaultdict(int), 
-        }
-    
-    def _get_incremental_constraints(self, step_constraints):
-        incremental_constraints = self._get_constraint_holder()
-
-        # handle green
-        for i in range(self.word_length):
-            if not self.constraints['green'][i] and step_constraints['green'][i]:
-                incremental_constraints['green'][i] = step_constraints['green'][i]
-        
-        # handle yellow
-        for i in range(self.word_length):
-            if not step_constraints['yellow'][i]:
-                continue
-            for step_yellow in step_constraints['yellow'][i]:
-                if step_yellow not in self.constraints['yellow'][i]:
-                    incremental_constraints['yellow'][i].add(step_yellow)
-
-        # handle gray
-        incremental_constraints['gray'] = step_constraints['gray'] - self.constraints['gray']
-
-        # handle count
-        for k, v in step_constraints['min_count'].items():
-            if self.constraints['min_count'][k] < v:
-                incremental_constraints['min_count'][k] = v
-        
-        for k, v in step_constraints['max_count'].items():
-            if self.constraints['max_count'].get(k, float('inf')) > v:
-                incremental_constraints['max_count'][k] = v
-
-        return incremental_constraints
-    
-    def _update_possible_answers(self, step_constraints):
-        incremental_constraints = self._get_incremental_constraints(step_constraints)
-
-        def _filter(word):
-            for i in range(self.word_length):
-
-                # handle green
-                if (
-                    self.constraints['green'][i] is not None
-                    and self.constraints['green'][i] != word[i]
-                ):
-                    return False
-                
-                # handle yellow
-                if word[i] in self.constraints['yellow'][i]:
-                    return False
-                
-                # continue here
-                ...
-                
-            return True
-
-        self.possible_answers = filter(_filter, self.possible_answers)
 
     def add_guess(self, guess):
         assert len(guess) == self.word_length
-        if guess not in self.valid_words:
-            print("Invalid guess")
-            return
 
         self.guesses.append(guess)
-        step_constraints = self._get_constraint_holder() # constraints specifically from this new guess
         taken = [False] * self.word_length # specify which letters are used up for count constraint
+        new_feedback = ['black'] * self.word_length
 
         # handle green
         for i in range(self.word_length):
             if guess[i] == self.answer[i]:
-                step_constraints['green'][i] = guess[i]
-                step_constraints['min_count'][guess[i]] += 1
+                new_feedback[i] = 'green'
                 taken[i] = True
         
         # handle yellow
-        for i in range(self.word_length):
-            if (guess[i] != self.answer[i] and guess[i] in self.letter_index):
-                for j in self.letter_index[guess[i]]:
-                    if taken[j]:
-                        continue
-                    step_constraints['yellow'][i].add(guess[i])
-                    step_constraints['min_count'][guess[i]] += 1
+        for i in range(self.word_length): # loop guess
+            for j in range(self.word_length): # loop answer
+                if guess[i] == self.answer[j] and not taken[j]:
+                    new_feedback[i] = 'yellow'
                     taken[j] = True
-                    break
 
-                # if all is taken, then it is the maximum count
-                else:
-                    step_constraints['max_count'][guess[i]] = step_constraints['min_count'][guess[i]]
-
-        
-        # handle gray
-        for i in range(self.word_length):
-            if guess[i] not in self.letter_index:
-                step_constraints['gray'].add(guess[i])
-        
-        self._update_possible_answers(step_constraints)
+        self.feedback.append(new_feedback)
+        return new_feedback
 
 if __name__ == "__main__":
-    wordle = Wordle()
-    print(len(wordle.valid_words))
-    print(len(wordle.valid_answers))
+    wordle = Wordle(answer='later')
+    feedback = wordle.add_guess('troll')
+    print(feedback)
+    
